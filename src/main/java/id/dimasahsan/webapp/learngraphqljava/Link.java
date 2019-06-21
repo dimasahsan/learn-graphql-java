076c290d33a312f342fae09bf4aeeaef75ad6489
Dimas Maulana A <dimasmaulanaahsan@gmail.com>
2019-06-21 11:22:00 +0700

    Update Link model

--- src/main/java/id/dimasahsan/webapp/learngraphqljava/Link.java
+++ src/main/java/id/dimasahsan/webapp/learngraphqljava/Link.java
@@ -1,14 +1,21 @@
 package id.dimasahsan.webapp.learngraphqljava;
 
 public class Link {
+
+    private final String id;
     private final String url;
     private final String description;
 
-    public Link(String url, String description) {
+    public Link(String id, String url, String description) {
+        this.id = id;
         this.url = url;
         this.description = description;
     }
 
+    public String getId() {
+        return id;
+    }
+    
     public String getUrl() {
         return url;
     }
