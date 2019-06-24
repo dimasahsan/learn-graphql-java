6d96fc31f810f0e977cebd4dcb54ea99a29968bf
Dimas Maulana A <dimasmaulanaahsan@gmail.com>
2019-06-24 14:48:18 +0700

    Override createContext from SimpleGraphQLServlet

--- src/main/java/id/dimasahsan/webapp/learngraphqljava/GraphQLEndpoint.java
+++ src/main/java/id/dimasahsan/webapp/learngraphqljava/GraphQLEndpoint.java
@@ -4,9 +4,13 @@ import com.coxautodev.graphql.tools.SchemaParser;
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoDatabase;
 import graphql.schema.GraphQLSchema;
+import graphql.servlet.GraphQLContext;
 import graphql.servlet.SimpleGraphQLServlet;
 
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Optional;
 
 @WebServlet(urlPatterns = "/graphql")
 public class GraphQLEndpoint extends SimpleGraphQLServlet {
@@ -35,5 +39,16 @@ public class GraphQLEndpoint extends SimpleGraphQLServlet {
                         new SignInResolver())
                 .build()
                 .makeExecutableSchema();
+    }
+
+    @Override
+    protected GraphQLContext createContext(Optional<HttpServletRequest> request, Optional<HttpServletResponse> response) {
+        User user = request
+                .map(req -> req.getHeader("Authorization"))
+                .filter(id -> !id.isEmpty())
+                .map(id -> id.replace("Bearer ", ""))
+                .map(userRepository::findById)
+                .orElse(null);
+        return new AuthContext(user, request, response);
     }
 }
