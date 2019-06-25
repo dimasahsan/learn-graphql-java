9f4084211696122cffeeb3da5062065526aafd35
Dimas Maulana A <dimasmaulanaahsan@gmail.com>
2019-06-25 10:56:24 +0700

    Add ZonedDateTime scalar type

---
+++ src/main/java/id/dimasahsan/webapp/learngraphqljava/Scalars.java
@@ -0,0 +1,32 @@
+package id.dimasahsan.webapp.learngraphqljava;
+
+import graphql.language.StringValue;
+import graphql.schema.Coercing;
+import graphql.schema.GraphQLScalarType;
+
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Scalars {
+
+    public static GraphQLScalarType dateTime = new GraphQLScalarType("DateTime", "DateTime scalar", new Coercing() {
+        @Override
+        public String serialize(Object input) {
+            return ((ZonedDateTime)input).format(DateTimeFormatter.ISO_OFFSET_DATE_TIME);
+        }
+
+        @Override
+        public Object parseValue(Object input) {
+            return serialize(input);
+        }
+
+        @Override
+        public ZonedDateTime parseLiteral(Object input) {
+            if (input instanceof StringValue) {
+                return ZonedDateTime.parse(((StringValue) input).getValue());
+            } else {
+                return null;
+            }
+        }
+    });
+}
